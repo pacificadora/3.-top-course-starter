a41cce6be93ce0bf912dc6a5000f688b00711ebc
Aman Singhal <amansinghal@Amans-MacBook-Air.local>
2024-10-06 12:08:22 +0530

    added the logic for showing different cards

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const App = () => {
 
   const [courses, setCourses] = useState([])
   const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
 
   async function fetchData() {
     setLoading(true);
@@ -30,10 +31,10 @@ const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <div><Navbar/></div>
-      <div className="bg-bgDark2">
-        <div><Filter filterData = {filterData} /></div>
+      <div className="bg-bgDark2 h-[100vh]">
+        <div><Filter filterData = {filterData} category={category} setCategory={setCategory}/></div>
         <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
-          {loading ? (<Spinner/>) : (<Cards courses = {courses}/>)}
+          {loading ? (<Spinner/>) : (<Cards courses = {courses} category={category}/>)}
         </div>
       </div>
     </div>
